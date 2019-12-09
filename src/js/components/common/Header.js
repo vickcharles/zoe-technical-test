35710969bf09bdebd9519e36f991d95f3abfd182
vickler charles <charlesvikler@gmail.com>
2019-12-08 19:51:06 -0500

    added styles to header

--- src/js/components/common/Header.js
+++ src/js/components/common/Header.js
@@ -1,7 +1,20 @@
 import React from "react";
 
+import { AppBar, Toolbar, Typography, Container } from "@material-ui/core";
+import "../../../assets/sass/header.sass";
+
 const Header = () => {
-  return <h1>header goes here</h1>;
+  return (
+    <AppBar position="fixed">
+      <Container>
+        <Toolbar className="tool-bar">
+          <Typography variant="h6" className="title">
+            Zoe Test
+          </Typography>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
 };
 
 export default Header;
