1bce1d30f32aac321cebadd89b0f691a52ea439f
vickler charles <charlesvikler@gmail.com>
2019-12-10 17:20:28 -0500

    added toggle to show more agents

--- src/js/components/agents/AgentsList.js
+++ src/js/components/agents/AgentsList.js
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import AgentCard from "../agents/AgentCard";
 import { Grid, Typography } from "@material-ui/core";
 
 const AgentsList = () => {
   const agents = useSelector(state => state.agents);
+  const [agentsToShow, setAgentsToShow] = useState(3);
+
+  const toggleShowMore = () => {
+    if (agentsToShow === 3) {
+      setAgentsToShow(agents.matchAgents.length);
+    } else {
+      setAgentsToShow(3);
+    }
+  };
   return (
     <div>
       <Typography variant="h2" className="primary-color JennaSue-Font">
@@ -12,11 +21,16 @@ const AgentsList = () => {
       </Typography>
       <Grid container>
         <Grid xs={6}>
-          {agents.matchAgents.map(agent => (
+          {agents.matchAgents.slice(0, agentsToShow).map(agent => (
             <AgentCard {...agent} />
           ))}
         </Grid>
       </Grid>
+      <div className="mt-medium mb-medium" onClick={toggleShowMore}>
+        <span className="grey-color cursor-pointer">
+          show {agentsToShow === 3 ? "more" : "less"}
+        </span>
+      </div>
     </div>
   );
 };
--- src/js/components/views/Agents.js
+++ src/js/components/views/Agents.js
@@ -1,11 +1,30 @@
 import React from "react";
 import AgentsList from "../agents/AgentsList";
-import { Container } from "@material-ui/core";
+import { Container, Typography, Button } from "@material-ui/core";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 const Agents = () => {
+  const agents = useSelector(state => state.agents);
+  const emptyList = (
+    <div className="text-center">
+      <Typography variant="h2" className="primary-color JennaSue-Font">
+        there is not match : (
+      </Typography>
+      <Typography variant="subtitle1">
+        No available Agents based on your income. Please try different income
+        value.
+      </Typography>
+      <Link to="/">
+        <Button variant="contained" color="secondary" className="mt-medium">
+          TRY AGAIN
+        </Button>
+      </Link>
+    </div>
+  );
   return (
     <Container>
-      <AgentsList />
+      {agents.matchAgents.length > 0 ? <AgentsList /> : emptyList}
     </Container>
   );
 };
